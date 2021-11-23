9aecead713e806b408fa168fe058bb3ee543d799
mreid21 <n01437978@unf.edu>
2021-11-23 13:30:28 -0500

    map courses

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles/App.scss';
 import Semester from './components/Semester';
-import CourseGraph from './assets/CourseGraph';
 
 function App() {
 
-  const [coursesTaken, setTaken] = useState([])
+  const [coursesTaken, setTaken] = useState(new Map())
 
-  const courseMap = new Map()
-  let courseGraph = new CourseGraph()
+  //setTaken(coursesTaken.set('Spring 2022', [computingCourses[0], computingCourses[1]]))
+  //setTaken(coursesTaken.set('Fall 2022', [computingCourses[2], computingCourses[3]]))
 
-  courseMap.set('COP2220', {courseId: 'COP2220', name: 'Programming I', credits: 3, prereqs: []})
-  courseMap.set('COT3100', {courseId: 'COT3100', name: 'Computational Structures', credits: 4, prereqs: []})
-  courseMap.set('COP3503', {courseId: 'COP3503', name: 'Programming II', credits: 3, prereqs: ['COP2220']})
-  courseMap.set('CDA3100', {courseId: 'CDA3100', name: 'Intro to Architecture', credits: 4, prereqs: ['COP2220']})
-  courseMap.set('COP3210', {courseId: 'COP3210', name: 'Theory of Computation', credits: 3, prereqs: ['COT3100']})
-  courseMap.set('COP3530', {courseId: 'COP3530', name: 'Data Structures', credits: 3, prereqs: ['COP3503']})
-  courseMap.set('COT4400', {courseId: 'COT4400', name: 'Design and Analysis of Algo', credits: 3, prereqs: ['COP3503', 'COT3100']})
-
-  courseGraph.constructGraph(courseMap)
-  courseGraph.display()
+  const computingCourses = [
+    {UUID: 35223553, courseId: 'COP2220', name: 'Programming I', credits: 3, prereqs: []},
+    {UUID: 19024908, courseId: 'COT3100', name: 'Computational Structures', credits: 4, prereqs: []},
+    {UUID: 19325081, courseId: 'COP3503', name: 'Programming II', credits: 3, prereqs: ['COP2220']},
+    {UUID: 12490963, courseId: 'CDA3100', name: 'Intro to Architecture', credits: 4, prereqs: ['COP2220']},
+    {UUID: 35909503, courseId: 'COP3210', name: 'Theory of Computation', credits: 3, prereqs: ['COT3100']},
+    {UUID: 13509899, courseId: 'COP3530', name: 'Data Structures', credits: 3, prereqs: ['COP3503']},
+    {UUID: 13580984, courseId: 'COT4400', name: 'Design and Analysis of Algo', credits: 3, prereqs: ['COP3503', 'COT3100']}
+  ]
 
   //TODO: fix this to do something right for once
+
+  const mapped = Array.from(coursesTaken).map(([key, val]) => <Semester term={key} courses={val}/>)
 
   return (
     <main className="container">
       <div className="course-container">
-        
+        {mapped}
       </div>
     </main>
   );
--- src/components/Semester.js
+++ src/components/Semester.js
@@ -2,13 +2,13 @@
 import '../styles/App.scss'
 import Course from './Course';
 
-const Semester = ({courses}) => {
+const Semester = ({courses, term}) => {
 
     return(
         <section class="glass-box course-box">
             <div class="course-box__content">
                 <header class="course-box__header">
-                    <p>Spring 2023</p>
+                    <p>{term}</p>
                     <p>X</p>
                 </header>
                 {courses.map((course) => <Course course={course}/>)}
